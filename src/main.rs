87297f6280b683e22ca27cf2543f315f38b0f9c9
Luke Flores <lflores30005@gmail.com>
2024-07-28 20:58:10 -0400

    update how arguments are handeled and add --help option

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,20 @@
 use std::env;
 use calc::basics::*;
 
+fn printhelp(){
+    println!("Terminal based calculator");
+    println!("Options(based on input arguments): ");
+    println!("\t --help, prints this message");
+    println!("\t a math expression, solves it and prints the result");
+    println!("\t no arguments, enters the mainscreen ui");
+    println!("The program uses standard pemdas to evaluate expressions, modulus is same priority as multiplication and division");
+    println!("The program has support for: ");
+    println!("\t operators: +, -, *(multiplication), /(division), %(modulus), and ^(exponentiation)");
+    println!("\t functions: sin(sine, all trig functions are in radians), cos(cosine), tan(tangent), floor, ceil(ceiling), round, ln(natural logarithm), and abs(absolute value, NOT done via '|')");
+    println!("\t Some functions use multiple parameters and are seperated via a comma(like f(x, y)) they include: log(logarithm first parameter is the base second is the argument), root(nth root of a number; first parameter is the n value, and the second is the number being taken the root of)");
+    println!("\t variables: e(euler's number), and pi");
+}
+
 fn main() {
     let mut argv = env::args();
     let argc = argv.len();
@@ -27,16 +41,22 @@ fn main() {
         mainscreen::ui();
     }
     else{
-        let mut input: String = String::new();
-        for arg in argv{
-            input.push_str(arg.as_str());
-            input.push(' ');
+        let args: Vec<String> = argv.collect();
+        if args[0] == "--help"{
+            printhelp();
         }
-        //algorithm adds an extra space this removes it
-        input.pop();
-        match fundamentals::solve(input){
-            Ok(n) => println!("{n}"),
-            Err(e) => eprintln!("{e}"),
+        else {
+            let mut input: String = String::new();
+            for arg in args{
+                input.push_str(arg.as_str());
+                input.push(' ');
+            }
+            //algorithm adds an extra space this removes it
+            input.pop();
+            match fundamentals::solve(input){
+                Ok(n) => println!("{n}"),
+                Err(e) => eprintln!("{e}"),
+            }
         }
     }
 }
